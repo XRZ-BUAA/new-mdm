f6f970a5a9f1025f4066170199f1f8f68171e303
XRZ-BUAA <22371261@buaa.edu.cn>
2024-08-07 22:02:08 +0800

    Debug predict_sparse in test.py

--- test.py
+++ test.py
@@ -93,16 +93,21 @@ class BodyModel(torch.nn.Module):
 
 def predict_sparse(args, sparse_original):
     predicted_sparse = sparse_original
-    sparse_per_frame = sparse_original[-1, :]
+    sparse_per_frame = sparse_original[:, -1, :]
     frame_count = 0
     while frame_count < args.predict_length:
-        sparse_per_frame[:, :18] = predicted_sparse[-1, :18] + predicted_sparse[-1, 18:36]
-        ang_acc = predicted_sparse[-1, 18:36] - predicted_sparse[-2, 18:36]
-        sparse_per_frame[:, 18:36] = predicted_sparse[-1, 18:36] + ang_acc
-        sparse_per_frame[:, 36:45] = predicted_sparse[-1, 36:45] + predicted_sparse[-1, 45:54]
-        linear_acc = predicted_sparse[-1, 45:54] - predicted_sparse[-2, 45:54]
-        sparse_per_frame[:, 45:54] = predicted_sparse[-1, 45:54] + linear_acc
-        predicted_sparse = torch.cat((predicted_sparse, sparse_per_frame), dim=0)
+        print("Sparse Per Frame Shape")
+        print(sparse_per_frame.shape)
+        print("Predicted Sparse Shape")
+        print(predicted_sparse)
+        
+        sparse_per_frame[:, :, :18] = predicted_sparse[:, -1, :18] + predicted_sparse[:, -1, 18:36]
+        ang_acc = predicted_sparse[:, -1, 18:36] - predicted_sparse[:, -2, 18:36]
+        sparse_per_frame[:, :, 18:36] = predicted_sparse[:, -1, 18:36] + ang_acc
+        sparse_per_frame[:, :, 36:45] = predicted_sparse[:, -1, 36:45] + predicted_sparse[:, -1, 45:54]
+        linear_acc = predicted_sparse[:, -1, 45:54] - predicted_sparse[:, -2, 45:54]
+        sparse_per_frame[:, :, 45:54] = predicted_sparse[:, -1, 45:54] + linear_acc
+        predicted_sparse = torch.cat((predicted_sparse, sparse_per_frame), dim=1)
         frame_count += 1
     return predicted_sparse
 
@@ -141,10 +146,10 @@ def non_overlapping_test(
     head_motion = head_motion.cuda().float()
     num_frames = head_motion.shape[0]
 
-    print("GT Data Shape")
-    print(gt_data.shape)
-    print("Sparse Original Shape")
-    print(sparse_original.shape)
+    # print("GT Data Shape")
+    # print(gt_data.shape)
+    # print("Sparse Original Shape")
+    # print(sparse_original.shape)
 
     output_samples = []
     count = 0
